84d225f27e0c9be6fb3edf26cde2dd50e472bcad
amey-shivaami <amey.gothankar@shivaami.com>
2023-10-13 07:06:32 +0000

    Added New file

---
+++ googleApi.py
@@ -0,0 +1,45 @@
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+SCOPES = ['https://www.googleapis.com/auth/admin.directory.user']
+
+def main():
+    
+    creds = None
+
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('admin', 'directory_v1', credentials=creds)
+
+    # Call the Admin SDK Directory API
+    print('Getting the first 10 users in the domain')
+    results = service.users().list(customer='my_customer', maxResults=10,
+                                   orderBy='email').execute()
+    users = results.get('users', [])
+
+    if not users:
+        print('No users in the domain.')
+    else:
+        print('Users:')
+        for user in users:
+            print(u'{0} ({1})'.format(user['primaryEmail'],
+                                      user['name']['fullName']))
+
+if __name__ == '__main__':
+    main()
