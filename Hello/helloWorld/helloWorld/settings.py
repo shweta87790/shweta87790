649d638f0911d2f3fb1afb6a4d4b1f81de334934
amey-shivaami <amey.gothankar@shivaami.com>
2023-10-16 13:08:23 +0000

    Updated allowed host

--- Hello/helloWorld/helloWorld/settings.py
+++ Hello/helloWorld/helloWorld/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0e%cbnv3al-a0^4(u+@peuc6w64(980@kx*p6t=!y6f1u02nfe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['35.200.161.77']
 
 
 # Application definition
