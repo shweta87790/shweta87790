8c8082c6ecd9f8cfc765f3332497a513032ce9be
amey-shivaami <147622646+amey-shivaami@users.noreply.github.com>
2023-10-16 13:02:49 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,33 @@
+resource "google_compute_instance" "test-build" {
+  project                   = "Internal POCs- Cloud Team"
+  name                      = "linux-amey"
+  machine_type              = "e2-medium"
+  zone = "asia-south1-c"
+  allow_stopping_for_update = "true"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-2004-focal-v20230918"
+    }
+  }
+  network_interface {
+    subnetwork         = "default"
+    subnetwork_project = "default"
+    access_config      = {}
+  }
+  metadata {
+    ssh-keys = "${{secrets.DEPLOY_KEY}}"
+  }
+
+provisioner "file" {
+  source = "HelloWorld.py"
+  destination = "/"
+
+  connection {
+    type = "ssh"
+    user = "amey123"
+    private_key = "${{secrets.PublicKey}}"
+    agent = "false"
+  }
+}
+}
