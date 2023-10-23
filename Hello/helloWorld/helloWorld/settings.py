1a1f03718714fbf4ea4cb690183e911d2bca8cb9
amey-shivaami <amey.gothankar@shivaami.com>
2023-10-23 04:30:40 +0000

    removed logging

--- Hello/helloWorld/helloWorld/settings.py
+++ Hello/helloWorld/helloWorld/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0e%cbnv3al-a0^4(u+@peuc6w64(980@kx*p6t=!y6f1u02nfe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['35.200.161.77']
 
@@ -122,8 +122,4 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-<<<<<<< HEAD
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-=======
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
->>>>>>> 9f41401f974dce306dd543e58f56aa8dee5368bd
